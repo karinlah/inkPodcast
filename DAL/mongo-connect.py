42d174d374e6d788929e2f2833997a12f08a85a0
karinlahyani <karinl@simplex.com>
2019-07-29 22:44:36 +0300

    DAL

--- DAL/mongo-connect.py
+++ DAL/mongo-connect.py
@@ -1,8 +1,14 @@
-# mongodb+srv://karin:<password>@cluster0-ub2ti.mongodb.net/test?retryWrites=true&w=majority
 import pymongo
 from bson.json_util import dumps
 
 client = pymongo.MongoClient("mongodb+srv://karin:Afeq1012@cluster0-ub2ti.mongodb.net/test?retryWrites=true&w=majority")
 db = client.test
-podcasts = db.podcasts
+podcasts_col = db.podcasts
 # dumps(podcast)
+
+
+# returning range of podcast the answering the query
+def get_podcasts_by_keyword(keywork):
+    return podcasts_col.find({'$text': {'$search': keywork}})
+
+
