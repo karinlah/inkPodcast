af5dd9e89c6390c23206acb71f58dee3ad4177d3
romdudk <rom.dudk@gmail.com>
2019-07-29 15:55:45 -0700

    added mp3 file fetching

--- DAL/mongo_connect.py
+++ DAL/mongo_connect.py
@@ -11,4 +11,24 @@ podcasts_col = db.podcasts
 def get_podcasts_by_keyword(keywork):
     return podcasts_col.find({'$text': {'$search': keywork}})
 
+def get_mp3_for_podcast(podcast_name, episode_name, output_file):
+    pod = podcasts_col.find({'name': podcast_name}).next()
+    url = ""
+    for episode in pod['episodes']:
+        if episode["title"] == episode_name:
+            url = episode['link']
 
+    if url =="":
+        return
+    import wget
+    wget.download(url, 'temp.html')
+    with open('temp.html', 'r') as g:
+        import re
+        pattern = re.compile("href=\"[^\"]*\.mp3\"")
+
+        for i, line in enumerate(g):
+            for match in re.finditer(pattern, line):
+                # print(match.group()[8:-1])
+                wget.download("https:"+match.group()[6:-1], output_file)
+    import os
+    os.remove("temp.html")
